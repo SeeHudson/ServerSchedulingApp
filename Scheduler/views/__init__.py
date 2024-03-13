50b0800f2928ad72c95a07b91461de31cc27f8de
aataullah <ataullah@uwm.edu>
2024-03-13 00:17:37 -0500

    Working Code as of 3/12

--- Scheduler/views/__init__.py
+++ Scheduler/views/__init__.py
@@ -1,2 +1,7 @@
 from .dashboard import Dashboard
 from .login import Login
+from .serverScore import ServerScore
+from .viewAllUsers import AllUsers
+from .editPersonalInfo import EditPersonalInfo
+from .account import Account
+from .UpdateAvailability import UpdateAvailability
---
+++ Scheduler/views/viewAllUsers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import View
+from django.contrib.auth.forms import AuthenticationForm
+
+import Scheduler
+from Scheduler.models import Employee, User
+
+class AllUsers(View):
+    def get(self, request):
+        allUsers = User.objects.all()
+        form = AuthenticationForm
+        message = request.session.get("message")
+        request.session.delete("message")
+        if message == None:
+            message = ""
+        return render(request, 'Scheduler/allUsers.html', {'userList': allUsers, 'message': message})
